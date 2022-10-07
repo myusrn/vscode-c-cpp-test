84f2c95321ab18ed11158ba27f6287b93e8dc8c7
myusrn <myusrn@outlook.com>
2022-10-07 08:49:41 -0700

    enabling non-cmake and makefile test

--- cmake-test/main.cpp
+++ cmake-test/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
-    return 0;
-}
-
-// void sayHello() {
-//     std::cout << "Hello, world (from function)!\n";
-// }
-
 // int main(int, char**) {
-//     sayHello();
+//     std::cout << "Hello, world!\n";
 //     return 0;
 // }
+
+void sayHello() {
+    std::cout << "Hello, world (from function)!\n";
+}
+
+int main(int, char**) {
+    sayHello();
+    return 0;
+}
